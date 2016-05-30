cfc345c5aba4b72b598f1a129155018b1db256cf
mushkaev <mushkaev@module.ru>
2016-05-30 20:19:53 +0300

    happy birthday

---
+++ download.bat
@@ -0,0 +1,22 @@
+set https_proxy=http://mushkaev:sergpasw@proxy:80
+set  http_proxy=http://mushkaev:sergpasw@proxy:80
+
+bin\wget -nc http://sourceforge.net/projects/gnuwin32/files/coreutils/5.3.0/coreutils-5.3.0-bin.zip
+bin\wget -nc http://sourceforge.net/projects/gnuwin32/files/coreutils/5.3.0/coreutils-5.3.0-dep.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/diffutils/2.8.7-1/diffutils-2.8.7-1-bin.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/diffutils/2.8.7-1/diffutils-2.8.7-1-dep.zip
+bin\wget -nc https://sourceforge.net/projects/gnuwin32/files/findutils/4.2.20-2/findutils-4.2.20-2-bin.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/findutils/4.2.20-2/findutils-4.2.20-2-bin.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/findutils/4.2.20-2/findutils-4.2.20-2-dep.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/grep/2.5.4/grep-2.5.4-bin.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/grep/2.5.4/grep-2.5.4-dep.zip
+bin\wget -nc http://tartarus.org/~simon/putty-snapshots/x86/putty.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/make/3.81/make-3.81-bin.zip 
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/make/3.81/make-3.81-dep.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-bin.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-dep.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/tar/1.13-1/tar-1.13-1-bin.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/tar/1.13-1/tar-1.13-1-dep.zip
+bin\wget -nc http://www.7-zip.org/a/7za920.zip
+bin\wget -nc http://downloads.sourceforge.net/project/gnuwin32/unzip/5.51-1/unzip-5.51-1-bin.zip
+bin\wget -nc --no-check-certificate --content-disposition https://github.com/elisherer/cecho/archive/master.zip
---
+++ install.bat
@@ -0,0 +1,21 @@
+bin\unzip -u -o -q bin\myaddons.zip -d "%GNUWIN32_INSTALL_DIRECTORY%" >NUL 2>&1
+
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" coreutils-5.3.0-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" coreutils-5.3.0-dep.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" diffutils-2.8.7-1-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" diffutils-2.8.7-1-dep.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" findutils-4.2.20-2-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" findutils-4.2.20-2-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" findutils-4.2.20-2-dep.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" grep-2.5.4-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" grep-2.5.4-dep.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" putty.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" make-3.81-bin.zip 
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" make-3.81-dep.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" wget-1.11.4-1-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" wget-1.11.4-1-dep.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" tar-1.13-1-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" tar-1.13-1-dep.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%"\bin 7za920.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" unzip-5.51-1-bin.zip
+bin\unzip -u -o -q -d "%GNUWIN32_INSTALL_DIRECTORY%" cecho-master.zip
